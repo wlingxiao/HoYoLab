edc67912220711c0b9ba70b0f201f8cb6c26d3a2
wlingxiao <5276935+wlingxiao@users.noreply.github.com>
2024-08-05 23:35:21 +0800

    Genshin region

--- src/HoYoLab/Requests/Genshin.cs
+++ src/HoYoLab/Requests/Genshin.cs
@@ -14,10 +14,30 @@ public class GenshinDailyCheckInRequest : IRequest
     public Uri RequestUri() => new Uri("https://sg-hk4e-api.hoyolab.com/event/sol/sign?act_id=e202102251931481");
 }
 
-public class GenshinExchangeCdkeyRequest(string uid, string cdkey, string region) : IRequest
+public class GenshinExchangeCdkeyRequest(string uid, string cdkey, GenshinRegion region) : IRequest
 {
     public HttpMethod Method => HttpMethod.Get;
 
     public Uri RequestUri() => new Uri(
-        $"https://sg-hk4e-api.hoyoverse.com/common/apicdkey/api/webExchangeCdkey?uid={uid}&region={region}&lang=zh-tw&cdkey={cdkey}&game_biz=hk4e_global&sLangKey=en-us");
+        $"https://sg-hk4e-api.hoyoverse.com/common/apicdkey/api/webExchangeCdkey?uid={uid}&region={region.GetRegion()}&lang=zh-tw&cdkey={cdkey}&game_biz=hk4e_global&sLangKey=en-us");
+}
+
+public enum GenshinRegion : byte
+{
+    America,
+    Europe,
+    Asia,
+    TwHkMo
+}
+
+public static class GenshinRegionExtensions
+{
+    public static string GetRegion(this GenshinRegion region) => region switch
+    {
+        GenshinRegion.America => "os_usa",
+        GenshinRegion.Europe => "os_euro",
+        GenshinRegion.Asia => "os_asia",
+        GenshinRegion.TwHkMo => "os_cht",
+        _ => throw new IndexOutOfRangeException(nameof(region))
+    };
 }
